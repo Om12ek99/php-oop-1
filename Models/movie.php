f982ae9751388a20a2f3755921cb5dc81b2ec81c
Om12ek99 <omar.elkamili@gmail.com>
2024-06-05 16:54:40 +0200

    add: setter and getter of descriprion

--- Models/movie.php
+++ Models/movie.php
@@ -7,6 +7,7 @@ class Movies{
     private string $director;
     private int $year;
 
+    // nome
     // set del nome
     public function setNameMovie(string $index_name) {
         if (strlen($index_name) > 0) {              //controlla che l'esistenza del valore
@@ -18,6 +19,28 @@ class Movies{
     // get del nome
     public function getNameMovie() {
         return $this->name;
+    }
+
+    
+    //description
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
 
