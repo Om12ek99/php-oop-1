2e28e8f09d72b145d0e7e20b1cc41b1718313aa9
Om12ek99 <omar.elkamili@gmail.com>
2024-06-05 16:55:35 +0200

    add: setter and getter of year

--- Models/movie.php
+++ Models/movie.php
@@ -63,6 +63,27 @@ class Movies{
 
         return $this;
     }
+
+    //year
+    /**
+     * Get the value of year
+     */ 
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * Set the value of year
+     *
+     * @return  self
+     */ 
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
 }
 
 ?>
