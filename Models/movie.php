922b11e0d1bb8c34ee93ee2c0488c7ab67afa29a
Om12ek99 <omar.elkamili@gmail.com>
2024-06-05 16:52:14 +0200

    add: setter and getter del nome

--- Models/movie.php
+++ Models/movie.php
@@ -6,6 +6,19 @@ class Movies{
     private string $description;
     private string $director;
     private int $year;
+
+    // set del nome
+    public function setNameMovie(string $index_name) {
+        if (strlen($index_name) > 0) {              //controlla che l'esistenza del valore
+            $this->name = $index_name;
+        } else {
+            $this->name = "Nessun titolo presente";
+        }
+    }
+    // get del nome
+    public function getNameMovie() {
+        return $this->name;
+    }
 }
 
 ?>
