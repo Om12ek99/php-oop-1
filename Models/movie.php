ffb8eaf047dc7939bb8cd8369b964dab1b01e576
Om12ek99 <omar.elkamili@gmail.com>
2024-06-05 16:49:00 +0200

    add: added class Movies

---
+++ Models/movie.php
@@ -0,0 +1,11 @@
+<?php
+//definizione di classe movie
+class Movies{
+    //definizione degli attributi
+    private string $name;
+    private string $description;
+    private string $director;
+    private int $year;
+}
+
+?>
